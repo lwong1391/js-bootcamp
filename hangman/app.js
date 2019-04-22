db15585cb35b8fbb5e59dc2ebd9edcb8a3463294
LW <lwong1391@gmail.com>
2019-04-22 10:30:56 +1000

    Constructors

---
+++ hangman/app.js
@@ -0,0 +1,24 @@
+// Primitive value: string, number,boolean, nukk, undefined
+
+// Object: myObject --> Object.prototype ==> null
+// Array: myArray --> Array.prototype --> Object.prototype --> null
+// Function: myFunc --> Function.prototype --> Object.prototype --> null
+// String: myString --> String.prototype --> Object.prototype --> null
+// Number: myNumber --> Number.prototype --> Object.prototype --> null
+// Boolean:myBoolean --> Boolean.prototype --> Object.prototype --> null
+
+const puzzleEl = document.querySelector('#puzzle');
+const guessEl = document.querySelector('#guesses')
+
+const game1 = new Hangman('cat', 2) ;
+
+puzzleEl.textContent =  game1.getPuzzle(); 
+guessEl.textContent = game1.displayStatusMessage();
+
+window.addEventListener('keypress', (e) => { 
+    const guess = String.fromCharCode(e.charCode);
+    game1.makingGuess(guess);     
+    puzzleEl.textContent =  game1.getPuzzle(); 
+    guessEl.textContent = game1.displayStatusMessage();
+})
+
